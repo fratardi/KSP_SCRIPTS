a4b9a949226f7b1777f3d013f46a509bda9f2f09
Your Name <you@example.com>
2024-07-18 22:39:30 +0200

    rearangement de l'insta;; vraiment pas tres beau pour  le moment

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # KSP_SCRIPTS
-cd /home/$(whoami)/snap/steam/common/.local/share/Steam/steamapps/common
+https://github.com/KSP-CKAN/CKAN/blob/master/doc/apt-repo.md
+How to install CKAN
+Needed in the directory i think  
+kill $(pidof steam)
+## install the kos so you can get it to work 
+ the most downloaded one is working ok by itseld so far 
+
+## alledgedly you put the crafts in the right place and select it 
+## install telnet   
+    apt install telnet
+## to connect on telnet if for ex port  = 1234 
+TELNET LOCALHOST 1234
+## put the scripts in the right place 
+
+
+## use the pre-built rockets matching the names 
+## load boot.ks into the program
--- UbuntuInstall.sh
+++ UbuntuInstall.sh
@@ -1,4 +1,16 @@
 
+$WOKINGDIR ""
+$DESTINATION""
 
-echo $(pwd)
-# cd /home/ubuntu/snap/steam/common/.local/share/Steam/steamapps/common
+
+
+
+# remove the Scriptfile for yours
+rm -rf   "/home/$(whoami)/snap/steam/common/.local/share/Steam/steamapps/common/Kerbal Space Program/Ships/Script"
+cp -r "$(pwd)/Script/." "/home/$(whoami)/snap/steam/common/.local/share/Steam/steamapps/common/Kerbal Space Program/Ships/Script"
+
+cd   "/home/$(whoami)/snap/steam/common/.local/share/Steam/steamapps/common/Kerbal Space Program/Ships/Script/."
+
+
+#Copy rocketSet to dest 
+cp -r "$(pwd)/Ships/VAB/." "/home/$(whoami)/snap/steam/common/.local/share/Steam/steamapps/common/Kerbal Space Program/Ships/VAB/."
